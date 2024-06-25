3a07f4d80afafd2bea15a3476d75f7f4b2d078b1
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2024-06-25 22:15:45 +0900

    Remove token

--- functions/invite.js
+++ functions/invite.js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
 	const json = await context.request.json();
-	const body = `token=xoxp-14621318070-14615577188-1049012910006-db96d2159f5a7abd067399b76a09170c&email=${json.email}&resend=true`;
-	console.log(body);
+	const body = `token=${context.env.token}&email=${json.email}&resend=true`;
 	const response = await fetch('https://slack.com/api/users.admin.invite', {
 		method: 'POST',
 		headers: {
